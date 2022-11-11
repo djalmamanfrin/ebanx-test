c0c71fdc430fc0c189ad8291d574a7ea124e5de0
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-11 17:08:40 -0300

    DepositServiceTest and DepositService implemented

---
+++ app/Services/DepositService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Account;
+use App\Models\Type;
+use App\TypesEnum;
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Response;
+
+class DepositService
+{
+    const MINIMUM_ALLOWED_VALUE = 1;
+
+    private Account $account;
+    private float $amount;
+
+    public function __construct(Account $account, float $amount)
+    {
+        $this->account = $account;
+        $this->amount = $amount;
+    }
+}
---
+++ tests/Services/DepositServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Services;
+
+use App\Models\Account;
+use App\Services\AccountService;
+use App\Services\DepositService;
+use Tests\TestCase;
+
+class DepositServiceTest extends TestCase
+{
+}
