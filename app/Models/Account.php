fd439367cd1f1a8e1b5fc85d82a9498532be00a4
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-11 16:18:28 -0300

    HasFactory implemented in the Models

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
+    use HasFactory;
+
     protected $table = 'accounts';
 
     public function transactions(): HasMany
@@ -14,4 +17,12 @@ class Account extends Model
         return $this->hasMany(Transaction::class);
     }
 
+    public function getBalance(): int
+    {
+        $transactions = $this->transactions()->get();
+        if ($transactions->isEmpty()) {
+            return 0;
+        }
+        return $transactions->pluck('amount')->sum();
+    }
 }
