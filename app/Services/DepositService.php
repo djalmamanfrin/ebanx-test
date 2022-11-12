c82a326df47c2f63e367d1fd841f21fa3b4feda2
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-12 16:35:53 -0300

    persit method implemented in the DepositServive

--- app/Services/DepositService.php
+++ app/Services/DepositService.php
@@ -3,18 +3,25 @@
 namespace App\Services;
 
 use App\Models\Account;
+use App\Models\Event;
+use App\TypesEnum;
+use Illuminate\Support\Facades\DB;
+use InvalidArgumentException;
+use Throwable;
 
 class DepositService
 {
     const MINIMUM_ALLOWED_VALUE = 1;
 
     private Account $account;
+    private Event $event;
     private float $amount;
 
-    public function __construct(Account $account, float $amount)
+    public function __construct(Account $account, Event $event)
     {
         $this->account = $account;
-        $this->setAmount($amount);
+        $this->event = $event;
+        $this->amount = 0;
     }
 
     private function isTheMinimumAllowed(float $amount): bool
@@ -22,8 +29,45 @@ class DepositService
         return $amount >= self::MINIMUM_ALLOWED_VALUE;
     }
 
-    private function setAmount(float $amount): void
+    public function setAmount(float $amount): DepositService
     {
         $this->amount = $amount;
+        return $this;
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function persist(): bool
+    {
+        $amount = $this->amount;
+        if (!$this->isTheMinimumAllowed($amount)) {
+            $message = "The deposit amount informed must be greater than or equal to" . self::MINIMUM_ALLOWED_VALUE;
+            throw new InvalidArgumentException($message);
+        }
+
+        try {
+            DB::beginTransaction();
+            if (is_null($this->account->id)) {
+                $this->account->saveOrFail();
+            }
+
+            if (is_null($this->event->id)) {
+                $this->event->saveOrFail();
+            }
+
+            $attributes = [
+                'type_id' => TypesEnum::DEPOSIT_ID,
+                'account_id' => $this->account->id,
+                'event_id' => $this->event->id,
+                'amount' => $amount
+            ];
+            return $this->account->transactions()
+                ->make($attributes)
+                ->saveOrFail();
+        } catch (Throwable $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
 }
--- tests/Services/DepositServiceTest.php
+++ tests/Services/DepositServiceTest.php
@@ -3,64 +3,84 @@
 namespace Tests\Services;
 
 use App\Models\Account;
+use App\Models\Event;
 use App\Services\DepositService;
+use InvalidArgumentException;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class DepositServiceTest extends TestCase
 {
+    use DatabaseTransactions, DatabaseMigrations;
+
+    private DepositService $deposit;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $account = Account::factory()->make();
+        $event = Event::factory()->make();
+        $this->deposit = new DepositService($account, $event);
+    }
+
     public function test_methods_exist_in_deposit_service()
     {
         $this->assertTrue(method_exists(DepositService::class, 'isTheMinimumAllowed'));
         $this->assertTrue(method_exists(DepositService::class, 'setAmount'));
     }
 
-    /**
-     * @dataProvider account
-     */
-    public function test_return_is_the_minimum_allowed_method_is_boolean(Account $account)
+    public function test_return_is_the_minimum_allowed_method_is_boolean()
     {
         $amount = 0;
-        $deposit = new DepositService($account, $amount);
-        $result = $this->invokeNonPublicMethod($deposit, 'isTheMinimumAllowed', [$amount]);
+        $result = $this->invokeNonPublicMethod($this->deposit, 'isTheMinimumAllowed', [$amount]);
         $this->assertIsBool($result);
     }
 
-    /**
-     * @dataProvider account
-     */
-    public function test_is_the_minimum_allowed_method_not_acceptable_amount_equal_zero(Account $account)
+    public function test_is_the_minimum_allowed_method_not_acceptable_amount_equal_zero()
     {
         $amount = 0;
-        $deposit = new DepositService($account, $amount);
-        $result = $this->invokeNonPublicMethod($deposit, 'isTheMinimumAllowed', [$amount]);
+        $result = $this->invokeNonPublicMethod($this->deposit, 'isTheMinimumAllowed', [$amount]);
         $this->assertFalse($result);
     }
 
-    /**
-     * @dataProvider account
-     */
-    public function test_is_the_minimum_allowed_method_acceptable_amount_equal_one(Account $account)
+    public function test_is_the_minimum_allowed_method_acceptable_amount_equal_one()
     {
         $amount = 1;
-        $deposit = new DepositService($account, $amount);
-        $result = $this->invokeNonPublicMethod($deposit, 'isTheMinimumAllowed', [$amount]);
+        $result = $this->invokeNonPublicMethod($this->deposit, 'isTheMinimumAllowed', [$amount]);
         $this->assertTrue($result);
     }
 
-    /**
-     * @dataProvider account
-     */
-    public function test_is_the_minimum_allowed_method_acceptable_amount_gather_than_one(Account $account)
+    public function test_is_the_minimum_allowed_method_acceptable_amount_gather_than_one()
     {
         $amount = 2;
-        $deposit = new DepositService($account, $amount);
-        $result = $this->invokeNonPublicMethod($deposit, 'isTheMinimumAllowed', [$amount]);
+        $result = $this->invokeNonPublicMethod($this->deposit, 'isTheMinimumAllowed', [$amount]);
         $this->assertTrue($result);
     }
 
-    public function account(): array
+    public function test_error_in_depositing_value_is_equal_zero()
     {
-        $account = Account::factory()->make();
-        return [[$account]];
+        $this->expectException(InvalidArgumentException::class);
+        $message = "The deposit amount informed must be greater than or equal to" . DepositService::MINIMUM_ALLOWED_VALUE;
+        $this->expectExceptionMessage($message);
+        $this->deposit
+            ->setAmount(0)
+            ->persist();
+    }
+
+    public function test_success_in_depositing_value_is_equal_one()
+    {
+        $isDeposited = $this->deposit
+            ->setAmount(1)
+            ->persist();
+        $this->assertTrue($isDeposited);
+    }
+
+    public function test_success_in_depositing_value_is_greater_than_one()
+    {
+        $isDeposited = $this->deposit
+            ->setAmount(2)
+            ->persist();
+        $this->assertTrue($isDeposited);
     }
 }
