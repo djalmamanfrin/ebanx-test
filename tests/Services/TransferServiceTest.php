1eee53f810e1b65e69af4d0c7741f4b6e3d6c4c5
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-13 02:32:12 -0300

    Testing whether abstract transaction service methods are available in the transfer service

--- tests/Services/TransferServiceTest.php
+++ tests/Services/TransferServiceTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Services;
 
-use App\Models\Account;
-use App\Models\Event;
-use App\Services\DepositService;
-use App\Services\TransactionService;
-use InvalidArgumentException;
-use Laravel\Lumen\Testing\DatabaseMigrations;
-use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Services\TransferService;
 use Tests\TestCase;
 
 class TransferServiceTest extends TestCase
-{}
+{
+    public function test_whether_abstract_transaction_service_methods_are_available_in_the_transfer_service()
+    {
+        $this->assertTrue(method_exists(TransferService::class, 'checkingMinimumAllowed'));
+        $this->assertTrue(method_exists(TransferService::class, 'persist'));
+    }
+}
