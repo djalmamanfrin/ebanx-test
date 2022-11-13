9c97745eb87bcdb2db1318cd695798dc923fb2b1
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-13 12:19:40 -0300

    Testing if getOriginAccount exist and return account instance expected

--- app/Services/TransactionManager.php
+++ app/Services/TransactionManager.php
@@ -2,7 +2,12 @@
 
 namespace App\Services;
 
+use App\Models\Account;
+
 class TransactionManager
 {
-
+    public function getOriginAccount(): Account
+    {
+        return new Account();
+    }
 }
--- tests/Services/TransactionManagerTest.php
+++ tests/Services/TransactionManagerTest.php
@@ -2,7 +2,20 @@
 
 namespace Tests\Services;
 
-class TransactionManagerTest
-{
+use App\Models\Account;
+use App\Services\TransactionManager;
+use Tests\TestCase;
 
+class TransactionManagerTest extends TestCase
+{
+    public function test_if_methods_are_available_in_the_transaction_manager()
+    {
+        $this->assertTrue(method_exists(TransactionManager::class, 'getOriginAccount'));
+    }
+
+    public function test_if_methods_return_expected_instance()
+    {
+        $manager = new TransactionManager();
+        $this->assertInstanceOf(Account::class, $manager->getOriginAccount());
+    }
 }
