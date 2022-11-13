eb3a2f4f90e83ae108049108f73753c6358789fc
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-13 00:32:15 -0300

    Testing return has found method is boolean in the withdraw service

--- tests/Services/WithdrawServiceTest.php
+++ tests/Services/WithdrawServiceTest.php
@@ -2,11 +2,28 @@
 
 namespace Tests\Services;
 
+use App\Models\Account;
+use App\Models\Event;
+use App\Services\DepositService;
 use App\Services\WithdrawService;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class WithdrawServiceTest extends TestCase
 {
+    use DatabaseTransactions, DatabaseMigrations;
+
+    private DepositService $deposit;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $account = Account::factory()->make();
+        $event = Event::factory()->make();
+        $this->withdraw = new WithdrawService($account, $event);
+    }
+
     public function test_whether_abstract_transaction_service_methods_are_available_in_the_withdraw_service()
     {
         $this->assertTrue(method_exists(WithdrawService::class, 'isTheMinimumAllowed'));
@@ -17,5 +34,11 @@ class WithdrawServiceTest extends TestCase
     public function test_whether_has_found_method_is_available_in_the_withdraw_service()
     {
         $this->assertTrue(method_exists(WithdrawService::class, 'hasFound'));
+    }
+
+    public function test_return_has_found_method_is_boolean()
+    {
+        $amount = 0;
+        $this->assertIsBool($this->withdraw->hasFound($amount));
     }
 }
