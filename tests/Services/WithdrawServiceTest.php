ece32df306a742de40b97a013797d7fb4d13fc44
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-13 00:00:14 -0300

    Initial withdraw service test

---
+++ tests/Services/WithdrawServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Services;
+
+use App\Models\Account;
+use App\Models\Event;
+use App\Services\DepositService;
+use App\Services\TransactionService;
+use InvalidArgumentException;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class WithdrawServiceTest extends TestCase
+{}
