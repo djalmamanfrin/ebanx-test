959e5532848e978ca6ab8aa1e2d8c0e6c30a0a34
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-12 23:37:08 -0300

    Initial transaction service test

---
+++ app/Services/TransactionService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Account;
+use App\Models\Event;
+use App\TypesEnum;
+use Illuminate\Support\Facades\DB;
+use InvalidArgumentException;
+use Throwable;
+
+abstract class TransactionService
+{
+}
---
+++ tests/Services/TransactionServiceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Services;
+
+use App\Models\Account;
+use App\Models\Event;
+use App\Services\DepositService;
+use App\Services\TransactionService;
+use InvalidArgumentException;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class TransactionServiceTest extends TestCase
+{
+}
