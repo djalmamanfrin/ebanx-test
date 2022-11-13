b3858cdb8762bd86e747b97b5c5f0fad5d7ad745
Djalma Manfrin <djalma.manfrin@gmail.com>
2022-11-13 12:34:58 -0300

    AccountService implemented in the getOriginAccount method

--- app/Services/TransactionManager.php
+++ app/Services/TransactionManager.php
@@ -2,12 +2,22 @@
 
 namespace App\Services;
 
+use App\Models\Event;
 use App\Models\Account;
 
 class TransactionManager
 {
+    private Event $event;
+
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+    }
+
     public function getOriginAccount(): Account
     {
-        return new Account();
+        $originAccountId = $this->event->origin;
+        $service = new AccountService($originAccountId);
+        return $service->get();
     }
 }
